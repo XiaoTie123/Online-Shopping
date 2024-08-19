1b84532da4f206395ca11b83b5135ee877128e6b
Kyaw Naing Soe <kyawnaingsoe@advent-soft.com>
2024-08-19 15:18:34 +0630

    test

--- onlineShop.java
+++ onlineShop.java
@@ -1,4 +1,5 @@
-public static void main(args){
-    System.out.println("Hello World");
-    )
+public class onlineShop {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
 }
